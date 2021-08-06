64fce44fec945c308e744f9d48f16f83efa8cee3
LordIsgaroth <lordisgaroth@gmail.com>
2021-08-06 14:49:13 +0300

    Реализовано движение корабля игрока с ускорением и инерцией

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,35 +4,64 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
-{
-    private Rigidbody2D _rigidBody;
+{    
     private Vector2 _moveAxis;
-    private float _speed;
+    private float _rotateDirection;
+    private float _maxSpeed;
+    private float _currentSpeed;
+    private float _rotationSpeed;
+    private float _inertion = 0.025f;
+    private float _acceleration = 0.05f;
+    private bool _accelerated = false;
 
 
     // Start is called before the first frame update
     void Start()
-    {
-        _rigidBody = GetComponent<Rigidbody2D>();
-        _moveAxis = new Vector2();
-        _speed = 1;
+    {        
+        _moveAxis = new Vector2(0, 1);
+        _rotateDirection = 0;
+        _maxSpeed = 2f;
+        _currentSpeed = 0;
+        _rotationSpeed = 2f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(_moveAxis * _currentSpeed * Time.deltaTime);
+        transform.Rotate(new Vector3(0, 0, 45) * _rotateDirection * _rotationSpeed * Time.deltaTime);
     }
 
-    public void FixedUpdate()
+    private void FixedUpdate()
     {
-        _rigidBody.velocity = _moveAxis * _speed;
+        //Инерция
+        if (_currentSpeed > 0) _currentSpeed -= _inertion;
+        else if (_currentSpeed < 0) _currentSpeed = 0;
+
+        Accelerate();
+
+    }
+
+    private void Accelerate()
+    {
+        if(_accelerated)
+        {
+            if (_currentSpeed < _maxSpeed) _currentSpeed += _acceleration;
+            else if (_currentSpeed > _maxSpeed) _currentSpeed = _maxSpeed;
+        }        
+
+        Debug.Log(_currentSpeed);
     }
 
     public void OnMove(InputAction.CallbackContext context)
     {
-        _moveAxis = context.ReadValue<Vector2>();
+        //_moveAxis = context.ReadValue<Vector2>();
 
-        Debug.Log(_moveAxis);
+        _accelerated = !context.canceled;
+    }
+
+    public void OnLook(InputAction.CallbackContext context)
+    {
+        _rotateDirection = -context.ReadValue<Vector2>().x;
     }
 }
